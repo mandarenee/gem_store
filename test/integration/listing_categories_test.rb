bf2ba443af03f51bd48e9124d7a423e40ae5a920
Amanda Kom <mandareneekom@gmail.com>
2015-05-03 20:47:38 -0700

    cleaning up code

--- test/integration/listing_categories_test.rb
+++ test/integration/listing_categories_test.rb
@@ -1,24 +1,10 @@
 require 'test_helper'
 
 class ListingCategoriesTest < ActionDispatch::IntegrationTest
-  # setup do
-  #   Product.create!(name: 'Ruby', category: 'gem')
-  #   Product.create!(name: 'Diamond')
-  # end
 
   test "listing categories" do
     get '/api/categories', {}, { 'Accept' => 'application/json' }
     assert_equal 200, response.status
     assert_equal Mime::JSON, response.content_type
-
-    # assert_equal 1, json(response.body).size
   end
-
-  # test 'lists categories in XML' do
-  #   get '/categories', {}, { 'Accept' => 'application/xml' }
-  #   assert_equal 200, response.status
-  #   assert_equal Mime::XML, response.content_type
-
-    # assert_equal 1, Hash.from_xml(response.body).size
-  # end
 end
